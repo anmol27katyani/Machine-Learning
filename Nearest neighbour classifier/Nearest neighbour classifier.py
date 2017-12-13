57b2c2d2bcf2a5d8ffcac717677d81d382776359
mishraanmol <anmolmishra54@outlook.com>
2017-12-13 20:24:37 +0530

    Added function

--- Nearest neighbour classifier/Nearest neighbour classifier.py
+++ Nearest neighbour classifier/Nearest neighbour classifier.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as pltcd
 
 #Below is the Distance function required
-def dist(x,y)
+def dist(x,y):
     return np.sqrt(np.sum((x-y)**2))
 
 X_train= np.array([[1,1],[2,2.5],[3,1.2],[5.5,6.3],[6,9],[7,6]])
@@ -17,6 +17,6 @@ plt.show()
 #To compute distance from every point in X_train
 num=len(X_train)
 distance=np.zeroes(num) # Numpy array of zeroes
-for i in range(num)
+for i in range(num):
     distance[i]=dist(X_train[i],X_test)
 print(distance)
